277464c6e3010cecf3f3f832ddcbf03ed9737a75
Ned Zimmerman <ned@bight.dev>
2024-08-14 10:14:25 -0300

    fix: apply lint fixes

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,26 +2,26 @@ import { EleventyRenderPlugin } from "@11ty/eleventy";
 import footnotesPlugin from "eleventy-plugin-footnotes";
 
 export default function eleventy(eleventyConfig) {
-  eleventyConfig.addPlugin(EleventyRenderPlugin);
-  eleventyConfig.addPlugin(footnotesPlugin);
+    eleventyConfig.addPlugin(EleventyRenderPlugin);
+    eleventyConfig.addPlugin(footnotesPlugin);
 
-  eleventyConfig.addPassthroughCopy({
-    "src/admin/config.yml": "admin/config.yml",
-  });
+    eleventyConfig.addPassthroughCopy({
+        "src/admin/config.yml": "admin/config.yml"
+    });
 
-  eleventyConfig.addPassthroughCopy({
-    "src/assets": "assets",
-  });
+    eleventyConfig.addPassthroughCopy({
+        "src/assets": "assets"
+    });
 
-  return {
-    dir: {
-      input: "src",
-      includes: "_includes",
-      layouts: "_layouts",
-      data: "_data",
-    },
-    templateFormats: ["njk", "md", "css", "png", "jpg", "svg"],
-    htmlTemplateEngine: "njk",
-    markdownTemplateEngine: "njk",
-  };
+    return {
+        dir: {
+            input: "src",
+            includes: "_includes",
+            layouts: "_layouts",
+            data: "_data"
+        },
+        templateFormats: ["njk", "md", "css", "png", "jpg", "svg"],
+        htmlTemplateEngine: "njk",
+        markdownTemplateEngine: "njk"
+    };
 }
