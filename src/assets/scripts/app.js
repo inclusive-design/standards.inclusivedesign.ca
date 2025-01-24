5c172ece1e2f83b4b0801f4f0b4ff790dc8775a7
Ned Zimmerman <ned@bight.dev>
2025-01-24 14:32:35 -0400

    feat: use custom element for disclosure component
    Signed-off-by: Ned Zimmerman <ned@bight.dev>

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -3,3 +3,40 @@ menuButton.addEventListener("click", () => {
     const ariaExpanded = menuButton.ariaExpanded === "false";
     menuButton.ariaExpanded = ariaExpanded;
 });
+
+/**
+ * Based on code by Chris Ferdinandi, released under the MIT license.
+ * @see https://gomakethings.com/web-components-vs.-state-based-ui/
+ */
+customElements.define(
+    "inclusive-disclosure",
+    class extends HTMLElement {
+        constructor() {
+            super();
+
+            this.toggle = this.querySelector("[aria-controls]");
+            this.content = this.querySelector("[content]");
+
+            if (!this.toggle || !this.content) {
+                return;
+            }
+
+            this.toggle.removeAttribute("hidden");
+            this.toggle.setAttribute("aria-expanded", false);
+            this.content.setAttribute("hidden", "");
+
+            this.toggle.addEventListener("click", this);
+        }
+
+        handleEvent(event) {
+            event.preventDefault();
+            if (this.toggle.getAttribute("aria-expanded") === "true") {
+                this.toggle.setAttribute("aria-expanded", false);
+                this.content.setAttribute("hidden", "");
+            } else {
+                this.toggle.setAttribute("aria-expanded", true);
+                this.content.removeAttribute("hidden");
+            }
+        }
+    }
+);
