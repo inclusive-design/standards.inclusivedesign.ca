66e7520110c21eef1b9a366796ce8faa1e86d8ad
Daniel Cho <chosww@gmail.com>
2025-03-13 09:29:35 -0400

    fix: add filter-container dependency

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,5 +1,3 @@
-import "@zachleat/filter-container";
-
 const menuButton = document.getElementById("navigation-toggle");
 menuButton.addEventListener("click", () => {
     const ariaExpanded = menuButton.ariaExpanded === "false";
