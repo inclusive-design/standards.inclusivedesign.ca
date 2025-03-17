6ed9a794241ed5932d819718ed11f2a2ed36d19e
Daniel Cho <chosww@gmail.com>
2025-03-17 16:26:07 -0400

    feat: add filtered tags

--- src/assets/scripts/filter.js
+++ src/assets/scripts/filter.js
@@ -1,18 +1,39 @@
 import "@zachleat/filter-container";
 
+window.onload = () => {
+    renderFilterTags();
+};
+
 const filterApplyButton = document.getElementById("apply");
 filterApplyButton.addEventListener("click", () => {
+    renderFilterTags();
+});
+
+const renderFilterTags = () => {
     const filterTags = document.getElementById("filter-tags");
     filterTags.innerHTML = "";
-    const filterProject = document.getElementById("filter-project");
-    for (const option of filterProject.querySelectorAll("input[type='checkbox']:checked")) {
-        const checkbox = filterProject.querySelector(`label[for='${option.id}']`);
-        const filterTag = document.createElement("button");
-        filterTag.addEventListener("click", () => {
-            checkbox.click();
-            document.getElementById("apply").click();
+    const filters = document.getElementById("filters");
+    const checkedFilterOptions = filters.querySelectorAll("input[type='checkbox']:checked");
+    if (checkedFilterOptions.length > 0) {
+        const filterApplied = document.getElementById("filter-applied");
+        filterApplied.style.display = "block";
+        for (const option of checkedFilterOptions) {
+            const checkbox = filters.querySelector(`label[for='${option.id}']`);
+            const filterTag = document.createElement("button");
+            filterTag.className = `filter-tag ${option.name}`;
+            filterTag.addEventListener("click", () => {
+                checkbox.click();
+                filterApplyButton.click();
+            });
+            filterTag.innerHTML = checkbox.innerHTML;
+            filterTags.appendChild(filterTag);
+        }
+        const clearFiltersButton = document.createElement("button");
+        clearFiltersButton.innerHTML = "Clear filters";
+        clearFiltersButton.addEventListener("click", () => {
+            document.querySelectorAll('input[type="checkbox"]').forEach((checkbox) => (checkbox.checked = false));
+            filterApplyButton.click();
         });
-        filterTag.innerHTML = checkbox.innerHTML;
-        filterTags.appendChild(filterTag);
+        filterTags.appendChild(clearFiltersButton);
     }
-});
+};
