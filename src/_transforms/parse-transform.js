b516e28922511b372349f167a04fdb67d667a7d5
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-03-13 12:39:16 -0300

    fix: generate nested TOC from h2-h3 headings (#416)
    Resolves #407, resolves #408

--- src/_transforms/parse-transform.js
+++ src/_transforms/parse-transform.js
@@ -4,17 +4,38 @@ const parseTransform = (value, outputPath) => {
 	if (outputPath && outputPath.includes('.html')) {
 		const {document} = parseHTML(value);
 
-		const pageNavHeadings = document.querySelectorAll('main:has(nav) article h2');
-		if (pageNavHeadings.length > 0) {
-			const navContainer = document.querySelector('main nav #toc ul');
-			for (const heading of pageNavHeadings) {
-				if (heading.parentNode.tagName !== 'NAV') {
-					const link = document.createElement('a');
-					link.setAttribute('href', `#${heading.id}`);
-					link.innerHTML = heading.textContent;
-					const li = document.createElement('li');
-					li.append(link);
-					navContainer.append(li);
+		const pageNavHeadings = document.querySelectorAll('main:has(nav) article h2, main:has(nav) article h3');
+		const navContainer = document.querySelector('main nav #toc ul');
+		for (const heading of pageNavHeadings) {
+			const link = document.createElement('a');
+			link.setAttribute('href', `#${heading.id}`);
+			link.innerHTML = heading.textContent;
+			const li = document.createElement('li');
+			li.append(link);
+
+			if (heading.tagName === 'H2') {
+				li.dataset.level = '2';
+				navContainer.append(li);
+			}
+
+			if (heading.tagName === 'H3') {
+				li.dataset.level = '3';
+
+				/* Retrieve the last list item for an H2 */
+				const lastListItem = [...navContainer.querySelectorAll('li[data-level="2"]')].pop();
+
+				/* See if it contains an unordered list for nested H3s */
+				let subUl = lastListItem.querySelector('ul');
+
+				/* Add the H3 list item to the unordered list if it exists */
+				if (subUl) {
+					subUl.append(li);
+					/* Create a new unordered list containing the H3 list item and add it to the H2 list item. */
+				} else {
+					subUl = document.createElement('ul');
+					subUl.className = 'flow';
+					subUl.append(li);
+					lastListItem.append(subUl);
 				}
 			}
 		}
