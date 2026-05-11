d164dcc9634ec4c0f9e558a417bed47b3f7fe525
Daniel Cho <chosww@gmail.com>
2026-05-11 16:42:50 -0400

    feat: add glossary page

--- eleventy.config.js
+++ eleventy.config.js
@@ -47,6 +47,8 @@ export default function eleventy(eleventyConfig) {
 				.toSorted((a, b) => a.data.title.localeCompare(b.data.title)),
 		);
 
+		eleventyConfig.addCollection(`glossaries_${lang}`, collection => collection.getFilteredByGlob(`src/collections/glossaries/${lang}/*.md`).toSorted((a, b) => a.data.term.localeCompare(b.data.term)));
+
 		eleventyConfig.addCollection(`pages_${lang}`, collection => collection.getFilteredByGlob(`src/collections/pages/${lang}/*.md`));
 
 		eleventyConfig.addCollection(`projects_${lang}`, collection => collection.getFilteredByGlob(`src/collections/projects/${lang}/*.md`));
