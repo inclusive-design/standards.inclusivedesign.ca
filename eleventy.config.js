cfa255380cb6ce68512940a72b69bd22542c36b5
Daniel Cho <chosww@gmail.com>
2026-05-12 10:35:27 -0400

    fix: update based on PR feedback

--- eleventy.config.js
+++ eleventy.config.js
@@ -47,7 +47,7 @@ export default function eleventy(eleventyConfig) {
 				.toSorted((a, b) => a.data.title.localeCompare(b.data.title)),
 		);
 
-		eleventyConfig.addCollection(`glossaries_${lang}`, collection => collection.getFilteredByGlob(`src/collections/glossaries/${lang}/*.md`).toSorted((a, b) => a.data.term.localeCompare(b.data.term)));
+		eleventyConfig.addCollection(`terms_${lang}`, collection => collection.getFilteredByGlob(`src/collections/terms/${lang}/*.md`).toSorted((a, b) => a.data.term.localeCompare(b.data.term)));
 
 		eleventyConfig.addCollection(`pages_${lang}`, collection => collection.getFilteredByGlob(`src/collections/pages/${lang}/*.md`));
 
