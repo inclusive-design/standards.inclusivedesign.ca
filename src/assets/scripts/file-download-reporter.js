b93c8c1260af3f3e6309407e588e12a4953ab1be
Justin Obara <obara.justin@gmail.com>
2026-05-26 14:01:37 -0400

    feat: track downloads

---
+++ src/assets/scripts/file-download-reporter.js
@@ -0,0 +1,13 @@
+// Based on download tracking script from https://docs.umami.is/docs/guides/track-file-downloads
+document.addEventListener('DOMContentLoaded', () => {
+	const fileExtensions = /\.(pdf|zip|tar|gz|rar|doc|docx|xls|xlsx|ppt|pptx|csv|mp3|mp4|dmg|exe|iso)$/iv;
+	const anchors = document.querySelectorAll('a[href]');
+
+	for (const anchor of anchors) {
+		if (fileExtensions.test(anchor.href) && !anchor.dataset.dataUmamiEvent) {
+			const filename = anchor.href.split('/').pop().split('?')[0];
+			anchor.dataset.dataUmamiEvent = 'file-download';
+			anchor.dataset.dataUmamiEventFile = filename;
+		}
+	}
+});
